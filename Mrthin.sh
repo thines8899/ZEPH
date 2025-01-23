cb0f5d1410c81982784f2404849c8d3e216d4b80
thines8899 <90979274+thines8899@users.noreply.github.com>
2025-01-24 05:25:19 +0800

    Update Mrthin.sh

--- Mrthin.sh
+++ Mrthin.sh
@@ -8,7 +8,7 @@ declare -A region_image_map=(
 )
 
 # URL containing User Data on GitHub
-user_data_url="https://raw.githubusercontent.com/thines8899/ZEPH/refs/heads/main/viZEPH"
+user_data_url="https://raw.githubusercontent.com/thines8899/ZEPH/refs/heads/main/vizephcli"
 
 # Path to User Data file
 user_data_file="/tmp/user_data.sh"
